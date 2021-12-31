af36b7987a258d699bdc1d9fd24857e7e14510a0
youssef316 <76614697+youssef316@users.noreply.github.com>
2021-12-31 03:27:47 +0300

    Add files via upload

---
+++ project web/example/javascript.js
@@ -0,0 +1,57 @@
+﻿  function checkForm(form)
+  {
+	  var f = 0;
+    if(form.uname.value == "") {
+      alert("Error: Username cannot be blank!");
+      form.uname.focus();
+      return false;
+    }
+    re = /^\w+$/;
+    if(!re.test(form.uname.value)) {
+      alert("Error: Username must contain only letters, numbers and underscores!");
+      form.uname.focus();
+      return false;
+    }
+
+    if(form.psw.value != "") {
+      if(form.psw.value.length < 6) {
+        alert("Error: Password must contain at least six characters!");
+        form.psw.focus();
+        return false;
+      }
+      if(form.psw.value == form.uname.value) {
+        alert("Error: Password must be different from Username!");
+        form.psw.focus();
+        return false;
+      }
+      re = /[0-9]/;
+      if(!re.test(form.psw.value)) {
+        alert("Error: password must contain at least one number (0-9)!");
+        form.psw.focus();
+        return false;
+      }
+      re = /[a-z]/;
+      if(!re.test(form.psw.value)) {
+        alert("Error: password must contain at least one lowercase letter (a-z)!");
+        form.psw.focus();
+        return false;
+      }
+      re = /[A-Z]/;
+      if(!re.test(form.psw.value)) {
+        alert("Error: password must contain at least one uppercase letter (A-Z)!");
+        form.psw.focus();
+        return false;
+      }
+    } else {
+      alert("Error: Please check that you've entered and confirmed your password!");
+      form.psw.focus();
+      return false;
+    }
+	f = 1;
+    alert("You entered a valid password: " + form.psw.value);
+	if ( f == 1)
+	{
+		loadDoc();
+	}
+    return true;
+  }
---
+++ project web/example/sessions.php
@@ -0,0 +1,11 @@
+﻿<?php
+session_start();
+?>
+<?php
+$_SESSION['user_name'] = "green";
+?>
+<?php
+if(isset($_SESSION['user_name'])){ // OR isset($_SESSION['user']), if array
+
+  	header('location: first_page.html');
+}
